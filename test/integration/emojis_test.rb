1377311bd359eb289b36bd24a3a65903b7144a19
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-07-25 18:32:02 +0200

    mock http request

--- test/integration/emojis_test.rb
+++ test/integration/emojis_test.rb
@@ -3,11 +3,12 @@ require 'test_helper'
 class EmojisTest < Minitest::Test
 
   def test_emojis
-    response = client.get('/emojis')
-
-    emoji = response.first
-    assert_equal "+1", emoji.symbol
-    assert_equal "https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png?v5", emoji.url
+    VCR.use_cassette("emojis") do
+      response = client.get('/emojis')
+      emoji = response.first
+      assert_equal "+1", emoji.symbol
+      assert_equal "https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png?v5", emoji.url
+    end
   end
 
   private
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'github_mapper'
 
 require 'minitest/autorun'
+
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+end
