509db322d6c4f17b12000afefe2eafbafbcf7b7f
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-07-26 14:33:43 +0200

    test response count

--- test/integration/emojis_test.rb
+++ test/integration/emojis_test.rb
@@ -4,8 +4,12 @@ class EmojisTest < Minitest::Test
 
   def test_emojis
     VCR.use_cassette("emojis") do
-      response = client.get('emojis')
-      emoji = response.first
+      emojis = client.get('emojis')
+
+      assert_equal 888, emojis.count
+
+      emoji = emojis.first
+
       assert_equal "+1", emoji.symbol
       assert_equal "https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png?v5", emoji.url
     end
--- test/integration/repositories_test.rb
+++ test/integration/repositories_test.rb
@@ -4,8 +4,11 @@ class RepositoriesTest < Minitest::Test
 
   def test_repositories
     VCR.use_cassette("repositories") do
-      response = client.get('repositories')
-      repository = response.first
+      repositories = client.get('repositories')
+
+      assert_equal 100, repositories.count
+
+      repository = repositories.first
 
       assert_equal 1, repository.id
       assert_equal "grit", repository.name
