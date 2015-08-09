c3e44f5cf4364b498324a12e816fc96d4ad3794a
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-08-09 14:24:04 +0200

    memorize client in test

--- test/support/client.rb
+++ test/support/client.rb
@@ -2,7 +2,7 @@ module TestClient
   private
 
   def client
-    ApiMapper::Client.new('https://api.github.com').tap do |client|
+    @client ||= ApiMapper::Client.new('https://api.github.com').tap do |client|
       client.router = GithubMapper::Router.new
     end
   end
